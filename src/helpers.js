f6084445bae3b512d3e1ddbfecb47c487a1d4963
Ricardo Ibarra <coder10@gmail.com>
2018-03-27 23:27:24 -0700

    Added array support

--- src/helpers.js
+++ src/helpers.js
@@ -14,11 +14,8 @@ export const deepCopy = (objRef) =>
   typeof objRef !== 'undefined' &&
   typeof objRef === 'object' ? JSON.parse(JSON.stringify(objRef)) : null;
 
-export const typeOfProp = (propName) => {
-  // put regex here for now
-  //figure out how to implement prop name
-  // someObj.someProp[2] = 21 or someObj.someProp = [321]
-  // see propertyTypes module
+const writeProp = (ref, propName, value) => {
+
 
 };
 
@@ -26,15 +23,30 @@ export const setProperty = (ref, propertyName, value) => {
   if (typeof propertyName === 'string' && propertyName.length > 0) {
     const nestedProps = propertyName.split('.');
     const top = nestedProps.length;
-    for (let c = 0; c < top; c++) {
-      let prop = nestedProps[c];
-      if ((c+1) === top)  {
-            ref[prop] = value;
-            break;
-      } else {
-        ref[prop] = {};
-      }
-    }
+    let _ref = ref;
+    const emptyArrayRegxp = /\[\]$/;
+    const indexArrayRegxp = /\[\d+\]$/;
+    const numberRegex = /^\d+$/;
+    nestedProps.forEach((prop, index) => {
+        if((index+1) === top) {
+          if (prop.match(emptyArrayRegxp)) {
+            const propName = prop.replace(emptyArrayRegxp, '');
+            _ref[propName] = [];
+            _ref[propName].push(value);
+            _ref = _ref[propName];
+          } else if(prop.match(indexArrayRegxp)) {
+            // i left here
+          } else { // simple prop name
+            _ref[prop] = value;
+          }
+
+        } else {
+          // have to check prop type here too.
+          _ref[prop] = {};
+          _ref = _ref[prop];
+        }
+    });
+
   } else {
     throw 'propertyName must be a string and lenght > 0';
   }
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -8,8 +8,37 @@ describe('Test deepify', () => {
     expect(total).equal(6);
   });
 
-  it('set a property', () => {
+  it('newely created object is not undefined', ()=> {
     const obj = deepify.set({}, 'person.last', 'ibarra');
     expect(obj.person).not.equal(undefined);
+  });
+
+  it('set a person.last = ibarra', () => {
+    const obj = deepify.set({}, 'person.last', 'ibarra');
+    expect(obj.person.last).equal('ibarra');
+  });
+
+  it('set deep prop country.person.last.phone = 42423432', () => {
+    const obj = deepify.set({}, 'country.person.last.phone', '42423432');
+    expect(obj.country.person.last.phone).equal('42423432');
+  });
+
+  it('set deep with special chars person.[$]sse = 1', () => {
+    const obj = deepify.set({}, 'person.[]sse', '1');
+    expect(obj.person['[]sse']).equal('1');
+  });
+
+  it('set array prop person.phones[] with item Hello in position 0 ', () => {
+    const obj = deepify.set({}, 'person.phones[]', 'hello');
+    expect(obj.person.phones instanceof Array).equal(true);
+    expect(obj.person.phones.length).equal(1);
+    expect(obj.person.phones[0]).equal('hello');
+  });
+
+  it('set array prop person.phones[2] with item 423432', () => {
+    const obj = deepify.set({}, 'person.phones[2]', '423432');
+    //expect(obj.person.phones instanceof Array).equal(true);
+    //expect(obj.person.phones.length).equal(3);
+    //expect(obj.person.phones[0]).equal('hello');
   });
 });
